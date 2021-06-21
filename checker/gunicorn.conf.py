7f8494b76e8f51cf823740a076312f783ecc4bc3
PistonMiner <pistonminer@gmail.com>
2021-06-21 23:00:59 +0200

    checker: Reconfigure gunicorn to minimize memory consumption

--- checker/gunicorn.conf.py
+++ checker/gunicorn.conf.py
@@ -1,7 +1,10 @@
 # This is a configuration file required by the checker.
 import multiprocessing
-worker_class = "eventlet"
-workers = multiprocessing.cpu_count()*2+1
+worker_class = "gevent"
+workers = multiprocessing.cpu_count() * 2 + 1
 bind = "0.0.0.0:3031"
 timeout = 90
 keepalive = 3600
+max_requests = 100
+preload_app = True
+max_requests_jitter = 30
